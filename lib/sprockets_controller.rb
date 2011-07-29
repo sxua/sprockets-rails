b36be7d61378f8b2146d267b21f88182d68e7e41
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2011-07-29 18:01:16 +0300

    uglify concatenated javascript

--- lib/sprockets_controller.rb
+++ lib/sprockets_controller.rb
@@ -2,6 +2,10 @@ class SprocketsController < ActionController::Base
   caches_page :show, :if => Proc.new { SprocketsApplication.use_page_caching }
   
   def show
-    render :text => SprocketsApplication.source, :content_type => "text/javascript"
+    render :text => uglify(SprocketsApplication.source), :content_type => "text/javascript"
+  end
+  
+  def uglify(text)
+    Uglifier.compile(text, :coyright => false, :mangle => false)
   end
 end
